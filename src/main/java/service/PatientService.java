7207fae2af470c9d142bce65519970e75e495611
迷局 星夜 <1014961529@qq.com>
2020-02-07 19:05:00 +0800

    新增web3j相关内容

---
+++ src/main/java/service/PatientService.java
@@ -0,0 +1,37 @@
+package service;
+
+import bean.Patient;
+import dao.Dao;
+import dao.DaoInterface;
+import ic_ethereum.IC_Administration;
+import ic_ethereum.Idus;
+import org.web3j.crypto.CipherException;
+import org.web3j.crypto.Credentials;
+import org.web3j.crypto.WalletUtils;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.http.HttpService;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.List;
+
+import static ic_ethereum.Idus.deploy;
+
+public class PatientService {
+    Dao dao;
+    public PatientService(){
+        dao=new Dao();
+    }
+    public List<Patient>selectVisitingRecord(String userid) throws Exception {
+        if(dao.selectIntelligentcontract() == null){
+            System.out.println("newIC!!!!!!");
+            String addr=IC_Administration.ICA.IC_deploy();
+            dao.addIntelligentcontract(addr);
+        }else{
+            System.out.println(dao.selectIntelligentcontract());
+        }
+        System.out.println("223333");
+        List<Patient> selall = dao.chadocbing(userid);
+        return selall;
+    }
+}
